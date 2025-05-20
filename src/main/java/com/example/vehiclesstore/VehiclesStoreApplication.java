fcf22227e0adf37fbe10f722cd4f71b3f5d3fd4d
rafaelpfernandes <rafael_perdigao80@hotmail.com>
2025-05-20 15:00:42 +0100

    Working fs

---
+++ src/main/java/com/example/vehiclesstore/VehiclesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehiclesstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vehiclesstore/VehiclesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehiclesstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
